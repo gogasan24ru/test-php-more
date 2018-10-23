8bdb3d575e28587880287385aeb53b963e8cad64
gogasan24ru <a@gogasan.tk>
2018-10-23 18:53:35 +0300

    Add main function

--- class.php
+++ class.php
@@ -2,7 +2,35 @@
 class PathBuilder
 {
         var $library;
-        function path($from,$to){}
+        function path($from,$to,$ret=Array()){
+		//$ret=$path;
+		//if (count($ret)==0)
+	        if(end($ret)!=$from)$ret[]=$from;
+		$ways=$this->findWays($from);
+		//print_r($ways);
+		//print_r($ret);
+		if(in_array($to,$ways))
+		{
+		        $ret[]=$to;
+                        return $ret;
+		}
+		if(count($ways)==1)return Array();
+		foreach ($ways as $way)
+		{
+			if(!in_array($way,$ret))
+			{
+				if($way===$to)
+				{
+				}
+				else
+				{
+					$ret=$this->path($way,$to,$ret);
+					if(end($ret)==$to)return $ret;
+				}
+			}
+		}
+		return $ret;
+	}
         function PathBuilder($path){
                 $library=Array();
                         try {
@@ -21,6 +49,18 @@ class PathBuilder
         {
                 return count($this->library);
         }
+
+	function findWays($source)
+	{
+		$ret=Array();
+		foreach ($this->library as $word)
+		{
+			if(levenshtein($word,$source)==1)
+				$ret[]=$word;
+		}
+		return $ret;
+	}
+
 }
 
 ?>
