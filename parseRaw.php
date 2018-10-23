4c8657f89699c7bc1349364d244fed31ee65d336
gogasan24ru <a@gogasan.tk>
2018-10-23 17:35:35 +0300

    Update parser

--- parseRaw.php
+++ parseRaw.php
@@ -3,12 +3,15 @@ mb_internal_encoding("UTF-8");
 $filename = $argv[1];
 $str=(file_get_contents($filename));
 $mathces=Array();
-preg_match_all('/>[а-яА-Я]{8}</', $str, $matches);
+preg_match_all('/>[А-Я]{8}</', $str, $matches);
 $matches=array_unique($matches[0]);
+
 foreach($matches as &$value)
 {
-	$value= strtolower(trim($value,'><'));
+	$value= mb_strtolower(trim($value,'><'));
+	file_put_contents("words.lib",$value.PHP_EOL,FILE_APPEND);
 }
+
 print_r($matches);
 
 ?>
